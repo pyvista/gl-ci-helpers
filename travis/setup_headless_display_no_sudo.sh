d1c095fbea62fd26ac27a429ad4f21ec11c07802
Bane Sullivan <banesullivan@gmail.com>
2019-04-24 16:05:43 -0600

    Create setup_headless_display_no_sudo.sh

---
+++ travis/setup_headless_display_no_sudo.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -x
+apt update && apt install python-qt4 libgl1-mesa-glx
+apt-get install -y xvfb
+export DISPLAY=:99.0
+export VTKI_OFF_SCREEN=True
+which Xvfb
+Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
+# Debugging commands:
+# ls -l /etc/init.d/
+# sh -e /etc/init.d/xvfb start
+# give xvfb some time to start
+sleep 3
+set +x
